c1b23c6749808e8d3baaa34c584322fc4a76b3eb
eduardo saldana <edusal100@hotmail.com>
2022-04-07 21:42:10 -0500

    resolving chart hover issues

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,50 +1,48 @@
 
 //chart data
-myData = {labels: ['10:00', '12:00', '14:00', '16:00', '18:00'],
-datasets: [{
-    data: [9, 10, 3, 19, 12],
-    borderColor: '#7FFFD4',
-    backgroundColor:'#7FFFD4',
-    fill: false
-}]
+let config = {
+    type: "line",
+    data: {
+        labels: ['10:00', '12:00', '14:00', '16:00', '18:00'],
+        datasets: [{
+            data: [9, 10, 3, 19, 12],
+            borderColor: '#7FFFD4',
+            backgroundColor:'#7FFFD4',
+            fill: false,
+}]},
+ options: { legend: {
+    display: false,
+},
+scales: {
+    yAxes: [{
+        gridLines: {
+            display: false,
+        },
+        ticks: {
+            display:false,
+        }   
+    }],
+    xAxes: [{
+        gridLines: {
+            borderDash: [8,4],
+        }
+    }] }
+    }
 };
 
-//chart options
-myOptions = { legend: {
-        display: false
-    },
-    scales: {
-        yAxes: [{
-            gridLines: {
-                display: false,
-            },
-            ticks: {
-                display:false,
-            }   
-        }],
-        xAxes: [{
-            gridLines: {
-                borderDash: [8,4],
-            }
-        }] }
-    }
 
-//Default chart with type: line
+//Default initialization of chart with type: line
 
 const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: myData, 
-    options: myOptions,
-    });
+let myChart = new Chart(ctx,config);
+    
 
 // Chart update function, destroys and re-render with line or bar depending on the selected option
 
-function updateChartType() {
+function changeChart(chartType) {
+ 
     myChart.destroy();
-    myChart = new Chart(ctx, {
-        type: document.getElementById("chartType").value,
-        data: myData,
-        options: myOptions,
-    });
+    config.type = chartType.value;
+    myChart = new Chart(ctx,config);
+
 }
