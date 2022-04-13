9e7988d53ef846328985c246f26036d62dc0c054
eduardo saldana <edusal100@hotmail.com>
2022-04-12 23:45:35 -0500

    adusting chart design with backgroun opacity

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,15 +1,25 @@
 
 // Data structure for Day, Week and Month
 
-const labels = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050]
+const labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 const data = {
     labels,
     datasets: [{
-        data: [86,114,106,106,107,111,133,221,783,2478],
+        data: [900,820,910,850,960,610,600,2500,2200,2600,2300,3000],
         label: "Africa",
         borderColor: "#3e95cd",
-    }]
+        fill: true,
+        backgroundColor: "rgb(62,149,205,0.1)",
+        pointRadius: 0,
+    }, {
+        data: [282,350,411,502,635,980,940,850,1000,1500,2000,2400],
+        label: "Asia",
+        borderColor: "#3e95cd",
+        borderDash: [6,6],
+        pointRadius: 0,
+    }
+]
 }
 
 //chart data object
@@ -17,9 +27,22 @@ const data = {
 const config = {
     type: "line",
     data: data,
+    options: {
+        plugins: {
+            legend: { 
+                display: false
+            }
+          },
+          scales: {
+              x: {
+                display: false
+              },
+              y: {
+                display: false
+              }
+          }
+    }
 }
-
-
 
 //Default initialization of chart with type: line
 
