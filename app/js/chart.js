ed1523aba58f1adae16ea859e636c44c806b4679
eduardo saldana <edusal100@hotmail.com>
2022-04-14 22:12:09 -0500

    added year week and month label changin on dom

--- app/js/chart.js
+++ app/js/chart.js
@@ -8,6 +8,9 @@ const data2 = [282,350,411,502,635,980,940,850,1000,1500,2000,2400]
 // Random arrays for week and month, using array push and mathfloor plus conditional
 
 let array_length;
+const monthLabel = [];
+const weekLabel = [];
+const yearLabel = [];
 const randomArrayMonth1 = [];
 const randomArrayMonth2 = [];
 const randomArrayWeek1 = [];
@@ -19,17 +22,19 @@ const data = {
     labels,
     datasets: [{
         data: data1,
-        label: "Africa",
+        label: "2022",
         borderColor: "#2d8dfe",
         fill: true,
         backgroundColor: "rgb(62,149,205,0.05)",
         pointRadius: 0,
+        pointHitRadius: 20,
     }, {
         data: data2,
-        label: "Asia",
+        label: "2021",
         borderColor: "#2d8dfe",
         borderDash: [6,6],
         pointRadius: 0,
+        pointHitRadius: 20,
     }
 ]
 }
@@ -72,7 +77,27 @@ function changeChart(chartType) {
 
 };
 
+// Current and previous Year, month and week
+const currentYear = new Date().getFullYear();
+const previousYear = currentYear - 1;
+const currentMonth = new Date().toLocaleDateString('en-us',{month:"long"});
+
+const current = new Date();
+current.setMonth(current.getMonth()-1);
+const previousMonth = current.toLocaleString('default', { month: 'long' });
+
+currentDate = new Date();
+    startDate = new Date(currentDate.getFullYear(), 0, 1);
+    const days = Math.floor((currentDate - startDate) /
+        (24 * 60 * 60 * 1000));
+
+const weekNumber = Math.ceil((currentDate.getDay() + 1 + days) / 7);
+const previousWeekNumber = Math.ceil((currentDate.getDay() + 1 + days) / 7)-1;
+
+
+
 // Period time update function: week, month and year with conditionals, array push for loops and event listener
+//Added an extra conditional to confirm if already have data or not to avoid keep pushing more to the array and use the alredy randonmly created data instead
 
 const selectElement = document.querySelector("#timeFrame");
 
@@ -84,6 +109,8 @@ function getRndInteger(min, max) {
 
 selectElement.addEventListener('change', (event) => {
     if (event.target.value === "month"){
+
+        if(randomArrayMonth1.length === 0 ){
         array_length = 30;
 
         for(let i=0; i<array_length; i++) {
@@ -92,9 +119,21 @@ selectElement.addEventListener('change', (event) => {
         }
         data.datasets[0].data = randomArrayMonth1;
         data.datasets[1].data = randomArrayMonth2;
+        document.querySelector('#current').innerHTML = currentMonth;
+        document.querySelector('#previous').innerHTML = previousMonth;
+        
+
+    } else {
+        data.datasets[0].data = randomArrayMonth1;
+        data.datasets[1].data = randomArrayMonth2;
+        document.querySelector('#current').innerHTML = currentMonth;
+        document.querySelector('#previous').innerHTML = previousMonth;
+    }   
 
     } if (event.target.value === "week") {
-        array_length = 5;
+
+        if(randomArrayWeek1.length === 0 ){
+        array_length = 12;
 
         for(let i=0; i<array_length; i++) {
             randomArrayWeek1.push(getRndInteger(10,150));
@@ -102,8 +141,20 @@ selectElement.addEventListener('change', (event) => {
         }
         data.datasets[0].data = randomArrayWeek1;
         data.datasets[1].data = randomArrayWeek2;
+        document.querySelector('#current').innerHTML = "W: " + weekNumber;
+        document.querySelector('#previous').innerHTML = "W: " + previousWeekNumber;
+
+    } else {
+        data.datasets[0].data = randomArrayWeek1;
+        data.datasets[1].data = randomArrayWeek2;
+        document.querySelector('#current').innerHTML = "W: " + weekNumber;
+        document.querySelector('#previous').innerHTML = "W: " + previousWeekNumber;
     }
-    if (event.target.value === "year"){
+
+} if (event.target.value === "year"){
+
+        if(randomArrayYear1.length === 0 ){
+
         array_length = 12;
 
         for(let i=0; i<array_length; i++) {
@@ -112,8 +163,16 @@ selectElement.addEventListener('change', (event) => {
         }
         data.datasets[0].data = randomArrayYear1;
         data.datasets[1].data = randomArrayYear2;
+        document.querySelector('#current').innerHTML = currentYear;
+        document.querySelector('#previous').innerHTML = previousYear;
+
+        } else {
+            data.datasets[0].data = randomArrayYear1;
+            data.datasets[1].data = randomArrayYear2;
+            document.querySelector('#current').innerHTML = currentYear;
+            document.querySelector('#previous').innerHTML = previousYear;
 
     }
-    myChart.update();
-
+}
+        myChart.update();
 })
